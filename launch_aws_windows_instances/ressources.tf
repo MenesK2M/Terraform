14477ad17f0430ea24b8e5aaab25fd6d9ab1397c
MenesK2M <117518027+MenesK2M@users.noreply.github.com>
2023-03-21 09:54:31 -0400

    Update ressources.tf

--- launch_aws_windows_instances/ressources.tf
+++ launch_aws_windows_instances/ressources.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "myfirst_ec2_from_terraform" {
-  ami               = var.ami_linux
+  ami               = var.ami_windows
   availability_zone = var.aws_availability_zone
   instance_type     = var.instance_type
-  key_name          = var.key_name_linux
-  security_groups   = var.security_groups_linux
+  key_name          = var.key_name_windows
+  security_groups   = var.security_groups_Windows
   tags = {
-    "Name" = "linux server"
+    "Name" = "Windows server"
   }
 }
