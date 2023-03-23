b44eec0f02ae21c39312da6c7135e8cc33b50791
MenesK2M <117518027+MenesK2M@users.noreply.github.com>
2023-03-23 10:01:24 -0400

    Update ressources.tf

--- create_iam_user_add_to_group/ressources.tf
+++ create_iam_user_add_to_group/ressources.tf
@@ -1,15 +1,15 @@
-resource "aws_iam_user" "myIamUser" {
+resource "aws_iam_user" "1erUser" {
   name = "Maxime"
 }
 
 resource "aws_iam_group" "mygroup" {
-  name = "HR"
+  name = "DevOpsEngineer"
 }
 
 resource "aws_iam_group_membership" "Maxime_group_membership" {
   name = "${aws_iam_group.mygroup.name}-${aws_iam_user.myIamUser.name}"
   users = [
-    aws_iam_user.myIamUser.name,
+    aws_iam_user.1erUser.name,
   ]
   group = aws_iam_group.mygroup.name
 }
