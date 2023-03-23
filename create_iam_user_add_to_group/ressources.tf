9cee6e14259dd502c07e9b8522ab6ab26ae026c4
MenesK2M <117518027+MenesK2M@users.noreply.github.com>
2023-03-23 10:36:12 -0400

    3/23/23 homework (#1)

--- create_iam_user_add_to_group/ressources.tf
+++ create_iam_user_add_to_group/ressources.tf
@@ -2,14 +2,30 @@ resource "aws_iam_user" "UserOne" {
   name = "Maxime"
 }
 
+resource "aws_iam_user" "UserTwo" {
+  name = "Haward"
+}
+
+resource "aws_iam_user" "UserTree" {
+  name = "Staline"
+}
+
+resource "aws_iam_user" "UserFour" {
+  name = "Francoise"
+}
+
+resource "aws_iam_user" "UserFive" {
+  name = "Ozoya"
+}
+
 resource "aws_iam_group" "mygroup" {
   name = "DevOpsEngineer"
 }
 
 resource "aws_iam_group_membership" "Maxime_group_membership" {
-  name = "${aws_iam_group.mygroup.name}-${aws_iam_user.UserOne.name}"
+  name = aws_iam_group.mygroup.name
   users = [
-    aws_iam_user.UserOne.name,
+    aws_iam_user.UserOne.name, aws_iam_user.UserTwo.name, aws_iam_user.UserTree.name, aws_iam_user.UserFour.name, aws_iam_user.UserFive.name
   ]
   group = aws_iam_group.mygroup.name
 }
